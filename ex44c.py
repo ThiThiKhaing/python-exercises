66ca16cc388a833bf3f75faa8697f04eb34e31a5
ThiThiKhaing <beliber1248@gmail.com>
2018-06-14 20:32:49 +0630

    add ex44c.py

--- ex44c.py
+++ ex44c.py
@@ -1,10 +1,14 @@
 class Parent(object):
-
+    def override(self):
+        print("PARENT override()")
+    def implicit(self):
+        print("PARENT implicit()")
     def altered(self):
         print("PARENT altered()")
 
 class Child(Parent):
-
+    def override(self):
+        print("CHILD override()")
     def altered(self):
         print("CHILD, BEFORE PARENT altered()")
         super(Child, self).altered()
@@ -12,6 +16,9 @@ class Child(Parent):
 
 dad = Parent()
 son = Child()
-
+dad.implicit()
+son.implicit()
+dad.override()
+son.override()
 dad.altered()
 son.altered()

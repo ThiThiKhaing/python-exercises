dd5999dff47b3014497f1bf49cab09300d8045e4
ThiThiKhaing <beliber1248@gmail.com>
2018-06-14 20:41:57 +0630

    add ex44d

--- ex44d.py
+++ ex44d.py
@@ -1,22 +1,23 @@
-class Parent(object):
+class Other(object):
     def override(self):
-        print("PARENT override()")
+        print("OTHER override()")
     def implicit(self):
-        print("PARENT implicit()")
+        print("OTHER implicit()")
     def altered(self):
-        print("PARENT altered()")
-class Child(Parent):
+        print("OTHER altered()")
+class Child(object):
+    def __init__(self):
+        self.other = Other()
+    def implicit(self):
+        self.other.implicit()
     def override(self):
         print("CHILD override()")
     def altered(self):
-        print("CHILD, BEFORE PARENT altered()")
-        super(Child, self).altered()
-        print("CHILD, AFTER PARENT altered()")
-dad = Parent()
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
 son = Child()
-dad.implicit()
 son.implicit()
-dad.override()
 son.override()
-dad.altered()
 son.altered()
